902c27f27754c7c06e7dcae302d8da06c7eb904b
Mahir <mahirshahariar647@gmail.com>
2024-10-21 15:46:48 +0600

    login step 1

--- js/login.js
+++ js/login.js
@@ -1,1 +1,10 @@
-console.log('object');
+document.getElementById("login-btn")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log('clicked');
+
+        const phoneNumber = document.getElementById('phn-number').value;
+        console.log(phoneNumber);
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(pinNumber);
+});

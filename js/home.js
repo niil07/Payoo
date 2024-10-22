7ecc362ef96aa02dd3d14d12335892496f893248
Mahir <mahirshahariar647@gmail.com>
2024-10-22 17:06:14 +0600

    cashout implement

--- js/home.js
+++ js/home.js
@@ -5,17 +5,32 @@ document.getElementById('input-add-btn')
         event.preventDefault();
 
         const addMoneyInput = document.getElementById('input-amount-btn').value;
-        console.log(addMoneyInput);
 
         const  pinInput = document.getElementById('input-pin-btn').value;
-        console.log(pinInput);
 
         const balance = document.getElementById('acc-balance').innerText;
-        console.log(balance);
 
         const addMoneyNumber = parseFloat(addMoneyInput);
         const balanceNumber = parseFloat(balance);
         const newBalance = addMoneyNumber+balanceNumber;
+
+        document.getElementById('acc-balance').innerText = newBalance;
+    })
+
+    document.getElementById('cashout-btn')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const cashoutInput = document.getElementById('cashout-amount-btn').value;
+
+        const  cashoutPinInput = document.getElementById('cashout-pin-btn').value;
+
+
+        const balance = document.getElementById('acc-balance').innerText;
+
+        const cashoutMoneyNumber = parseFloat(cashoutInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber - cashoutMoneyNumber;
 
         document.getElementById('acc-balance').innerText = newBalance;
     })
